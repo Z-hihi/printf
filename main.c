b4dfe42099f4b0592e118b1d551fe8855689554c
Yassmine <yassmine123fanid@gmail.com>
2023-04-23 15:47:33 -0700

    fixing issues

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	int count_std = 0, count_hmd = 0, count_cent = 0;
+	int count_std = 0, count_hmd = 0, count_cent = 0, len =0;
 
 	write(1, "STANDARD:\n", 10);
 	count_std = _printf("My favorite color is %s\n", "blue");
@@ -18,5 +18,7 @@ int main(void)
 	 _printf("there is %d character in the previous sentence\n", count_hmd);
 	count_cent = _printf("100%%\n");
 	_printf("there is %d character in the previous sentence\n", count_cent);
+	len += _printf("the number is: %d", 123);
+	_printf("there is %d character in the previous sentence\n", len);
 	return (0);
 }
