5ccfa81a1bf4ecc6f7c219f4caa132128eefa970
Yassmine <yassmine123fanid@gmail.com>
2023-04-23 13:19:01 -0700

    fixing main

--- main.c
+++ main.c
@@ -8,12 +8,10 @@
  */
 int main(void)
 {
-	int count_std = 0, count_hmd = 0;
-
 	write(1, "STANDARD:\n", 10);
-	count_std = _printf("My favorite color is %s\n", "blue");
+	_printf("My favorite color is %s\n", "blue");
 	write(1, "STANDARD:\n", 10);
-	count_hmd = _printf("The first letter of my name is: %c", 'Y');
+	_printf("The first letter of my name is: %c", 'Y');
 	_printf("100%");
 
 	return (0);
