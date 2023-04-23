51fdcd3f7a07c4b2f168423ab2841f8a5ece7892
Yassmine <yassmine123fanid@gmail.com>
2023-04-23 11:47:50 -0700

    specifier function for task 0

---
+++ check_specifiers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * check_specifier - checks that a character is a valid specifier and
+ * assigns an appropriate function for its printing
+ *@format: the specifier
+ *Return: pointer to function (Success), NULL(fails)
+ */
+int (*check_specifier(const char *format))(va_list)
+{
+	int i;
+
+	func_t my_array[6] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"%", print_cent},
+		{NULL, NULL}
+	};
+	for (i = 0; my_array[i].t != NULL; i++)
+	{
+		if (*(my_array[i].t) == *format)
+		{
+			return (my_array[i].f);
+		}
+	}
+	return (NULL);
+}
