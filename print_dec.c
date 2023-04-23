a65bf96bdbaaa9dc535a51fd005a4e4a36232a63
Yassmine <yassmine123fanid@gmail.com>
2023-04-23 14:08:57 -0700

    function to print decimals

--- print_dec.c
+++ print_dec.c
@@ -4,4 +4,27 @@
  * @args: variadic parameter
  * Return: number of characters printed(Success)
  */
+int print_dec(va_list args)
+{
+	int i = 0, count = 0, numb, my_arr[10];
+	char x[1];
 
+	numb = va_arg(args, int);
+	while (numb != 0)
+	{
+		my_arr[i] = numn % 10;
+		numb /= 10;
+		if (numb == 0)
+		{
+			break;
+		}
+		else
+			i++;
+	}
+	for (i; i >= 0; i--)
+	{
+		x[0] = ('0' + my_arr[i]);
+		count += write(1, x, 1);
+	}
+	return (count);
+}
